e3eaae57917a1241b33d2f3ffd26858d8abfb270
Didenko17 <vladislav.didenko.2001@gmail.com>
2021-01-10 23:10:49 +0300

    Header, Footer и SearchComponent перевёрстаны на antd. Небольшие фиксы в функционале.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
+import 'antd/dist/antd.css'
 import {AppRedirect} from './components/AppRedirect';
 import {Link} from 'react-router-dom';
+import { Layout, Breadcrumb } from 'antd';
+
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <Link className='header-link' to='/'><h1>Github Dashboard</h1></Link>
-      <AppRedirect/>
+      <Layout className="layout">
+      <Header>
+      <Link className='logo' to='/'>Github Dashboard</Link>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">
+          <AppRedirect/>
+        </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
     </div>
   );
 }
--- client/src/components/SearchComponent.js
+++ client/src/components/SearchComponent.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import {Link} from 'react-router-dom';
 import axios from "axios";
+import { Input, Table, Pagination} from 'antd';
 
-const getRepository = (text,page) => {
-  const reqPage=page>0?Math.ceil(page*10/30):1;
+const {Search} = Input;
+
+const getRepositories = (text,page) => {
+  //API возвращает по 30 реп на страницу, отрисовывается 10 реп на страницу
+  const reqPage=page>3?Math.ceil(page*10/30):1;
   const query=text?'q='+text+'+':'';
   return axios("https://api.github.com/search/repositories?q=stars:>0&"+query+"in:name&sort=stars&order=desc&page="+reqPage, {
     method: "GET",
@@ -11,41 +15,46 @@ const getRepository = (text,page) => {
       Authorization: "token fe9b07660dce9da2777b4f148ae8dd20f976f3aa",
     },
   }).then((res) => {
+    //Выбираем из 30 возвращённых реп 10 нужных
     const start = reqPage*3-page===2?0:reqPage*3-page===1?10:20;
     return [res.data.items.slice(start,start+10),res.data.total_count]
   });
 };
-const formPageArr= (curPage)=>{
-  const arr=[];
-  if(curPage==1)
-    for(let i =curPage;i<curPage+10;i++){
-      arr.push(i);
-    }
-  else{
-    for(let i =curPage-1;i<curPage+9;i++){
-      arr.push(i);
-    }
-  }
-  return arr;
-}
-const RepoItem = ({item}) => {
-  
-  return (
-    <tr className="row">
-      <td className="repo-name">{item.name}</td>
-      <td className="stars">{item.stars}</td>
-      <td className="last-commit">{item.lastCommitDate}</td>
-      <Link to={'/repos/'+item.link}><td className="link">{item.url}</td></Link>
-    </tr>
-  );
-};
+
 export const SearchComponent = function () {
   const [loading, setLoading] = useState(true)
-  const [value, changeValue] = useState("");
-  const [list, setList] = useState([]);
-  const [page, setPage] = useState(1);
+  const [searchText, setSearchText] = useState("");
+  const [repos, setRepos] = useState([]);
+  const [curentPage, setCurrentPage] = useState(1);
+  const [lastRepoNum, setLastRepoNum]= useState(1000);
+  const columns=[
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text, row, index) => <Link to={'/repos/'+repos[index].link}>{text}</Link>,
+    },
+    {
+      title: 'Stars',
+      dataIndex: 'stars',
+      key: 'stars',
+    },
+    {
+      title: 'Last commit date',
+      dataIndex: 'lastCommitDate',
+      key: 'lastCommitDate',
+    },
+    {
+      title: 'URL',
+      dataIndex: 'url',
+      key: 'url',
+      render: url => <a target='blank' href= {url}>{url}</a>
+    },
+  ]
   const refetch = async () => {
-    const result = await getRepository(value,page);
+    //Получаем репозитории
+    const result = await getRepositories(searchText,curentPage);
+    //Оставляем только нужные данные
     const receivedList = result[0].map((a) => {
       return {
         name: a.name,
@@ -55,41 +64,25 @@ export const SearchComponent = function () {
         link:a.full_name
       };
     });
-    setList(receivedList);
-    setLoading(false)
+    setRepos(receivedList);
+    //Так как API выдаёт только первые 1000 реп, обрезаем лишние
+    if(result[1]>=1000){
+      setLastRepoNum(1000);
+    }else{
+      setLastRepoNum(result[1]);
+    }
+    setLoading(false);
   };
-  useEffect(() => void refetch(), [value,page]);
+
+  useEffect(() => void refetch(), [searchText,curentPage]);
   if(loading){
     return (<h2>Loading...</h2>);
   }
-  return (
+  return(
     <div>
-      <input
-        id="search"
-        onChange={(e) =>changeValue(e.target.value)}
-        placeholder="Search"
-      />
-      <table>
-        <thead>
-          <tr>
-            <th className="repo-name">Name</th>
-            <th className="stars">Stars</th>
-            <th className="last-commit">Last commit date</th>
-            <th className="link">URL</th>
-          </tr>
-        </thead>
-        <tbody>
-          {list.map((a) => <RepoItem item={a}/>)}
-        </tbody>
-      </table>
-      {formPageArr(page).map((a)=>{
-        return(
-          <button key={a} className='page-number' onClick={()=>{setPage(a)}}>
-            {a}
-          </button>
-        );
-      })
-      }
+      <Search style={{width:'50%', margin:'30px 25%'}} placeholder="input search text" onSearch={(value)=>setSearchText(value)} enterButton />
+      <Table pagination={false} dataSource={repos} columns={columns} />
+      <Pagination className='pagination' showQuickJumper defaultCurrent={1} total={lastRepoNum} onChange={(page)=>{setCurrentPage(page)}} />
     </div>
   );
 }
